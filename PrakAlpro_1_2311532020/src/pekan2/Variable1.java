de171edd513a7a978087e96ec7630d4a86b15c92
sonicmortis <luthfikautsarrizata@gmail.com>
2024-10-01 11:29:49 +0700

    praktikum2

---
+++ PrakAlpro_1_2311532020/src/pekan2/Variable1.java
@@ -0,0 +1,14 @@
+package pekan2;
+
+public class Variable1 {
+
+	public static void main(String[] args) {
+		int x;
+		x=3;
+		System.out.println("x adalah " + x); // x is 3
+		x=5*x-1;
+		System.out.println("x adalah " + x); //5 * 3 -1
+
+	}
+
+}
